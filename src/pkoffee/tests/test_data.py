0fec8d381c8a8922ae59acd319ae74c6c0ffb232
pbederke <paulbederke@gmail.com>
2026-01-16 12:26:29 +0100

    Add test for loading data

--- src/pkoffee/tests/test_data.py
+++ src/pkoffee/tests/test_data.py
@@ -3,3 +3,13 @@ def test_initial():
 
     assert 1 == 1
     #assert fibonacci(1) == 1
+
+
+def test_load_csv():
+    from pkoffee.data import load_csv
+    from pathlib import Path
+
+    df = load_csv(Path("analysis/coffee_productivity.csv"))
+    assert not df.empty
+    assert "productivity" in df.columns
+    assert "cups" in df.columns
